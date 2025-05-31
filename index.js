b169edc4d02496915543f9f2f93239641d9c0a5c
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-05-31 02:00:30 +0100

    added a env to gitignore

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import http from 'http'
+import http from 'http';
+const PORT = process.env.PORT;
 
 const Server = http.createServer((req,res) => {
     res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello Worldddddddd</h1>')
+    res.end('<h1>Hello Worldddddddd</h1>');
 })
 
-Server.listen(8080, () => {
-    console.log('waitThe Server is running')
+Server.listen(PORT, () => {
+    console.log(PORT)
+    console.log(`Your Server is running on ${PORT}`);
 })
