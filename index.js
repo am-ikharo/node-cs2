c50f71b5fadfe5edd106fba1281a986edce1b816
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-01 00:07:17 +0100

    created the public folder and 2 html files in it

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 import http from 'http';
+import fs from 'fs|promises';
+import url from 'url';
 const PORT = process.env.PORT;
 
 const Server = http.createServer((req,res) => {
-    if(req.url === '/'){
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>Home Page</h1>');
-    }else if(req.url === '/about'){
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>About Page</h1>');
-    }else{
+    try {
+        if(req.method === 'GET'){
+            if(req.url === '/'){
+                res.setHeader('Content-Type', 'text/html');
+                res.end('<h1>Home Page</h1>');
+            }else if(req.url === '/about'){
+                res.setHeader('Content-Type', 'text/html');
+                res.end('<h1>About Page</h1>');
+            }else{
+                res.setHeader('Content-Type', 'text/plain');
+                res.statusCode = 404;
+                res.end('Page not Found');
+            }
+        }else{
+            throw new Error('Method not found');
+        }
+        
+    } catch (error) {
         res.setHeader('Content-Type', 'text/plain');
-        res.statusCode = 404;
-        res.end('Page not Found');
+        res.statusCode = 500;
+        res.end('Server Error');
     }
+    
     
 })
 
